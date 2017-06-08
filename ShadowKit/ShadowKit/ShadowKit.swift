e25c79f21ef7eaa9a7da0bfbc514bb23e14785aa
Mayur <jhonmathews@gmail.com>
2017-06-08 21:16:27 +0530

    - Removed forced unwraps by making the APIs return optional UIImage objects - Added a standard synchronous and asynchronous API (not as a part of an extension on a type) - Added documentation to almost all functions

--- ShadowKit/ShadowKit/ShadowKit.swift
+++ ShadowKit/ShadowKit/ShadowKit.swift
@@ -7,18 +7,34 @@
 //
 import UIKit
 
-//TODO: Doccumentation
-public func shadowBlurImage(for image: UIImage) -> UIImage {
+/// Call this method to return a UIImage with the Shadow Blur effect applied.
+/// WARNING: This method returns synchronously. Using large images, or making multiple calls, will slow your app. applyShadowBlur(to:_:) offers better performance since it doesn't block the main thread.
+///
+/// - Parameter image:
+/// - Returns: An image formed by applying the Shadow Blur effect to the image given by the caller. If something goes wrong while constructing, or applying the blur, this method returns nil.
+public func shadowBlurImage(for image: UIImage) -> UIImage? {
     return applyBlurEffect(to: image)
+}
+
+
+/// Call this method with an image and a completion handler. Your completion handler is called with a UIImage that has the Shadow Blur effect applied to it.
+///
+/// - Parameters:
+///   - image: The image to which youd like to apply the shadowblur effect
+///   - completionHandler: Your completion handler is called on the main thread with the shadow blur effect applied to your image. If something goes wrong while constructing, or applying the blur, your completion handler is called with nil.
+public func applyShadowBlur(to image: UIImage, _ completionHandler: @escaping (UIImage?) -> Void) {
+    applyingBlurEffect(to: image) { (imageWithShdowBlur) in
+        completionHandler(imageWithShdowBlur)
+    }
 }
 
 extension UIImage{
     
-    /// Call this method on a UIImage to its shadow blurred image called in a completion handler.
+    /// Call this method on a UIImage to have its shadow blurred image called in a completion handler.
     /// The completion handler will be called asynchronously and on the main thread.
     ///
     /// - Parameter completionHandler: A callback recieved with the shadowblurred image as a parameter
-    public func applyingShadowBlur(_ completionHandler: @escaping (UIImage) -> Void) {
+    public func applyingShadowBlur(_ completionHandler: @escaping (UIImage?) -> Void) {
         applyingBlurEffect(to: self) { (shadowBlurredImage) in
             completionHandler(shadowBlurredImage)
         }
@@ -26,6 +42,7 @@ extension UIImage{
 }
 
 extension UIImageView{
+    //TODO: Doccumentation
     public func withShadowBlurApplied(_ completionHandler: @escaping (UIImage?) -> Void) {
         guard let image = self.image else { completionHandler(nil); return }
         
@@ -39,7 +56,7 @@ extension UIImageView{
 //TODO: Have an extension on a UIView that takes a 'screenshot' of the current view and returns a blurred image of that view (asynchronously)
 
 //TODO: Doccumentation
-func applyingBlurEffect(to image: UIImage, completionHander: @escaping (UIImage) -> Void) {
+func applyingBlurEffect(to image: UIImage, completionHander: @escaping (UIImage?) -> Void) {
     DispatchQueue.global(qos: .userInteractive).async {
         let blurredImage = applyBlurEffect(to: image)
         
@@ -51,22 +68,23 @@ func applyingBlurEffect(to image: UIImage, completionHander: @escaping (UIImage)
     }
 }
 
-//TODO: Make this method return an optional UIImage
 //TODO: Doccumentation
-func applyBlurEffect(to image: UIImage) -> UIImage {
+func applyBlurEffect(to image: UIImage) -> UIImage? {
     //TODO: Remove all forced-downcasts
     let context = CIContext(options: nil)
-    let imageToBlur = CIImage(image: image)
-    let blurfilter = CIFilter(name: "CIGaussianBlur")
-    blurfilter!.setValue(imageToBlur, forKey: "inputImage")
+    guard let imageToBlur = CIImage(image: image) else { return nil }
+    guard let blurfilter = CIFilter(name: "CIGaussianBlur") else { return nil }
+    blurfilter.setValue(imageToBlur, forKey: "inputImage")
     let blurRadius = 80.0
     let inset = CGFloat(blurRadius * 4.0)
-    blurfilter!.setValue(blurRadius, forKey: "inputRadius")
-    let resultImage = blurfilter!.value(forKey: "outputImage") as! CIImage
+    blurfilter.setValue(blurRadius, forKey: "inputRadius")
+    guard let resultImage = blurfilter.value(forKey: "outputImage") as? CIImage else {
+        return nil
+    }
     
     //What we hope to achieve at the end point it to return an image with a blur. This image should be slightly larger across bounds of the imageview, which will them super-impose the image on top of the blur, giving it the Apple Music album art effect.
     //Figure out the extents of both the original and the blurred images:
-    let origExtent = imageToBlur!.extent
+    let origExtent = imageToBlur.extent
     var newExtent = resultImage.extent
     
     //Normalise the bounds and center of the new extent to fit our requirement of 'slightly larger bounds than the original image'
@@ -77,8 +95,8 @@ func applyBlurEffect(to image: UIImage) -> UIImage {
     //...and then re-size to fit:
     newExtent.size = CGSize(width: origExtent.size.width + inset * reorientationFraction, height: origExtent.size.height + inset * reorientationFraction)
     
-    let cgImage = context.createCGImage(resultImage, from: newExtent)
-    let blurredImage = UIImage(cgImage: cgImage!)
+    guard let cgImage = context.createCGImage(resultImage, from: newExtent) else { return nil }
+    let blurredImage = UIImage(cgImage: cgImage)
     
     return blurredImage
 }
